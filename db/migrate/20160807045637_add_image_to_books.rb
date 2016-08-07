8f62d907ba785d66d844d7a873a24b16eafdcc56
YOUR NAMECristianAristizabal <cfaristizabalr@gmial.com>
2016-08-07 00:05:48 -0500

    delete a paperlip migration

---
+++ db/migrate/20160807045637_add_image_to_books.rb
@@ -0,0 +1,11 @@
+class AddImageToBooks < ActiveRecord::Migration
+  def change
+  	def self.up
+  		add_attachment :books, :image
+  	end
+
+  	def self.down
+  		remove_attachment :books, :image
+  	end
+  end
+end
