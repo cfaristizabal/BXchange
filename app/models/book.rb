f04b2ed516a923991c9079b2cde706a1b94e4a39
Cristian <cfaristizabalr@gmail.com>
2016-08-10 02:41:49 +0100

    Ready Devise

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+  belongs_to :user
 	has_attached_file :image, :styles => {:thumb => '100x100>', :square => '200x200#', :medium => '300x300>'}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
